5ad5dc1643ba0a5a34981a890cc741786c5785b4
Kemal Ilke Tuna <kemalilketuna@gmail.com>
2024-05-25 01:04:38 +0300

    init

---
+++ Quiz4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Quiz4 {
+    public static void main(String[] args) throws IOException {
+        
+        // TODO: Use the first and the second command line argument (args[0] and args[1]) to read the database and the query file.
+        // TODO: Your code goes here
+        // TODO: Print the solution to STDOUT
+    }
+}
